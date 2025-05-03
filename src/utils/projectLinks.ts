418cc07d6c207fed8272173cb4a41340a40e9ba3
M201200 <m201400@gmail.com>
2025-05-03 16:38:59 +0300

    replace all dead links

---
+++ src/utils/projectLinks.ts
@@ -0,0 +1,14 @@
+export const myLeisureLinks = {
+  github: "https://github.com/M201200/my-leisure",
+  page: "https://my-leisure.vercel.app/",
+}
+
+export const newFurnitureLinks = {
+  github: "https://github.com/M201200/new-furniture",
+  page: "https://new-furniture-mu.vercel.app/",
+}
+
+export const playgroundLinks = {
+  github: "https://github.com/M201200/the-playground",
+  page: "https://the-playground-sigma.vercel.app/",
+}
