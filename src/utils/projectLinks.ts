9e9cec37ef1c59132eb7ec1d5bf3d47bf756b27f
M201200 <m201400@gmail.com>
2025-06-08 14:55:46 +0300

    adding another project

--- src/utils/projectLinks.ts
+++ src/utils/projectLinks.ts
@@ -1,3 +1,37 @@
+export const loadhunterLinks: {
+  name: {
+    en: string
+    ru: string
+    ro: string
+  }
+  url: string
+}[] = [
+  {
+    name: {
+      en: "Dashboard",
+      ru: "Панель управления",
+      ro: "Panou de control",
+    },
+    url: "https://app.loadhunter.io/",
+  },
+  {
+    name: {
+      en: "Landing Page",
+      ru: "Лендинг",
+      ro: "Pagina de destinație",
+    },
+    url: "https://loadhunter.io/",
+  },
+  {
+    name: {
+      en: "Chrome Extension",
+      ru: "Расширение для Chrome",
+      ro: "Extensie Chrome",
+    },
+    url: "https://chromewebstore.google.com/detail/loadhunter/ogepjnnfghfpkpjjieenkcppifhmmcdg",
+  },
+]
+
 export const myLeisureLinks = {
   github: "https://github.com/M201200/my-leisure",
   page: "https://my-leisure.vercel.app/",
