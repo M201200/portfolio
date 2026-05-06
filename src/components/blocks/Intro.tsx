d8a94fcc60cce25a8861a65a12073b610d6a0a8f
M201200 <m201200dev@gmail.com>
2026-05-06 18:46:48 +0300

    add intro

---
+++ src/components/blocks/Intro.tsx
@@ -0,0 +1,10 @@
+export function Intro() {
+  return (
+    <header className="flex flex-col col-start-2 col-end-3 justify-center min-h-1/2">
+      <h1 className="text-7xl">Vlad Iuzhev</h1>
+      <p className="text-2xl">Front-end Lead</p>
+      <p className="text-base">I make it work.</p>
+      <a href="#main-projects">View projects</a>
+    </header>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
+import { Intro } from '#/components/blocks/Intro'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
+    <main className="grid grid-cols-[1fr_minmax(0,80ch)_1fr] min-h-screen">
+      <Intro />
+    </main>
   )
 }
